52f27df3d22c408c5f0612fd939374e5ab9f58cf
marcelocab200 <marcelocab200@gmail.com>
2025-02-20 10:52:06 -0300

    feat: first commit

---
+++ src/main/java/com/desafiomagalu/communication_api/CommunicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.desafiomagalu.communication_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiomagalu/communication_api/CommunicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiomagalu.communication_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
