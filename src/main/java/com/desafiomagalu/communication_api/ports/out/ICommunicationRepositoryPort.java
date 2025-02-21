06ee375fd2f13efc2e7b2cc89e21f6a845525b72
marcelocab200 <marcelocab200@gmail.com>
2025-02-21 04:21:45 -0300

    fix: repository port interface methods return type and name

--- src/main/java/com/desafiomagalu/communication_api/ports/out/ICommunicationRepositoryPort.java
+++ src/main/java/com/desafiomagalu/communication_api/ports/out/ICommunicationRepositoryPort.java
@@ -1,12 +1,14 @@
 package com.desafiomagalu.communication_api.ports.out;
 
+import java.util.Optional;
+
 import com.desafiomagalu.communication_api.application.domain.Communication;
 
 public interface ICommunicationRepositoryPort {
     
     public Communication saveCommunication(Communication communication);
 
-    public Communication changeCommunicationStatus(Communication communication);
+    public Communication updateCommunication(Long id);
 
-    public Communication getCommunication(Communication communication);
+    public Optional<Communication> getCommunication(Long id);
 }
