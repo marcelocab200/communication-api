d64bf199415381e2e95a76a4cf666468b1faa42f
marcelocab200 <marcelocab200@gmail.com>
2025-02-22 15:52:59 -0300

    fix: update method annotations and return type

--- src/main/java/com/desafiomagalu/communication_api/infrastructure/repository/ICommunicationJpaRepository.java
+++ src/main/java/com/desafiomagalu/communication_api/infrastructure/repository/ICommunicationJpaRepository.java
@@ -1,10 +1,16 @@
 package com.desafiomagalu.communication_api.infrastructure.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.desafiomagalu.communication_api.application.domain.CommunicationStatus;
 import com.desafiomagalu.communication_api.infrastructure.entity.CommunicationEntity;
 
 public interface ICommunicationJpaRepository extends JpaRepository<CommunicationEntity, Long> {
 
-    public CommunicationEntity updateById(Long id);
+    @Modifying
+    @Query(value = "UPDATE CommunicationEntity c SET c.status = :status WHERE c.id = :id")
+    Integer updateById(@Param("id") Long id, @Param("status") CommunicationStatus status);
 }
--- src/main/java/com/desafiomagalu/communication_api/ports/out/ICommunicationRepositoryPort.java
+++ src/main/java/com/desafiomagalu/communication_api/ports/out/ICommunicationRepositoryPort.java
@@ -3,12 +3,13 @@ package com.desafiomagalu.communication_api.ports.out;
 import java.util.Optional;
 
 import com.desafiomagalu.communication_api.application.domain.Communication;
+import com.desafiomagalu.communication_api.application.domain.CommunicationStatus;
 
 public interface ICommunicationRepositoryPort {
     
     public Communication saveCommunication(Communication communication);
 
-    public Communication updateCommunication(Long id);
+    public Optional<Communication> updateCommunication(Long id, CommunicationStatus status);
 
     public Optional<Communication> getCommunication(Long id);
 }
