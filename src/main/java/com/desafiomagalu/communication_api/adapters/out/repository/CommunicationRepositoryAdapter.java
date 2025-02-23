a69bc8ef9ad18d8717d391a13aea058ac3bb5fa7
marcelocab200 <marcelocab200@gmail.com>
2025-02-23 03:43:41 -0300

    feat: add info logs

--- src/main/java/com/desafiomagalu/communication_api/adapters/out/repository/CommunicationRepositoryAdapter.java
+++ src/main/java/com/desafiomagalu/communication_api/adapters/out/repository/CommunicationRepositoryAdapter.java
@@ -11,11 +11,12 @@ import com.desafiomagalu.communication_api.application.domain.CommunicationStatu
 import com.desafiomagalu.communication_api.infrastructure.repository.ICommunicationJpaRepository;
 import com.desafiomagalu.communication_api.ports.out.ICommunicationRepositoryPort;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class CommunicationRepositoryAdapter implements ICommunicationRepositoryPort {
 
     @Autowired
@@ -25,18 +26,37 @@ public class CommunicationRepositoryAdapter implements ICommunicationRepositoryP
 
     @Override
     public Communication saveCommunication(Communication communication) {
-        return mapper.toDomain(repository.save(mapper.toEntity(communication)));
+
+        log.info("Salvando comunicação no banco de dados...");
+
+        Communication saveResult = mapper.toDomain(repository.save(mapper.toEntity(communication)));
+
+        log.info("Comunicação salva com sucesso.");
+
+        return saveResult;
     }
 
     @Override
     public Optional<Communication> updateCommunication(Long id, CommunicationStatus status) {
+
+        log.info("Fazendo a alteração no banco de dados...");
+
         repository.updateById(id, status);
+
+        log.info("Status alterado com sucesso.");
 
         return getCommunication(id);
     }
 
     public Optional<Communication> getCommunication(Long id) {
-        return repository.findById(id).map(mapper::toDomain);
+
+        log.info("Buscando comunicação no banco de dados...");
+
+        Optional<Communication> getResult = repository.findById(id).map(mapper::toDomain);
+
+        log.info("Comunicação encontrada com sucesso.");
+
+        return getResult;
     }
     
 }
--- src/main/java/com/desafiomagalu/communication_api/application/services/CommunicationService.java
+++ src/main/java/com/desafiomagalu/communication_api/application/services/CommunicationService.java
@@ -1,5 +1,10 @@
 package com.desafiomagalu.communication_api.application.services;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.desafiomagalu.communication_api.application.domain.Communication;
@@ -9,36 +14,66 @@ import com.desafiomagalu.communication_api.ports.out.ICommunicationRepositoryPor
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CommunicationService implements ICommunicationServicePort {
 
+    @Autowired
     private ICommunicationRepositoryPort repository;
 
     @Override
-    public Communication scheduleCommunication(Communication communication) {      
-        communication.setDateHour(java.time.LocalDateTime.now()); 
+    @Transactional
+    public Communication scheduleCommunication(Communication communication) {     
+
+        log.info("Agendando comunicação...");
+
+        Date localDateTime2Date = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()); 
+
+        communication.setDateHour(localDateTime2Date); 
         communication.setStatus(CommunicationStatus.SCHEDULED);
-        return repository.saveCommunication(communication);
+
+        Communication scheduleResult = repository.saveCommunication(communication);
+
+        log.info("Comunicação agendada com sucesso.");
+
+        return scheduleResult;
     }
 
     @Override
+    @Transactional
     public Communication consultCommunication(Long id) {
-        return repository.getCommunication(id)
-            .orElseThrow(() -> new IllegalStateException("Comunicação não foi encontrada."));
+
+        log.info("Consultando comunicação...");
+
+        Communication consultResult = repository.getCommunication(id)
+        .orElseThrow(() -> new IllegalStateException("Comunicação não foi encontrada."));
+
+        log.info("Consulta efetuada com sucesso.");
+
+        return consultResult;
     }
     
     @Override
     @Transactional
     public Communication cancelCommunication(Long id) {
-        final Communication consultedCommunication = consultCommunication(id);
+
+        log.info("Cancelando comunicação...");
+
+        Communication consultedCommunication = consultCommunication(id);
 
         if (consultedCommunication.getStatus() == CommunicationStatus.CANCELED) {
+            log.error("Não foi possível cancelar a comunicação.");
             throw new IllegalStateException("A comunicação já foi cancelada.");
         }
 
-        return repository.updateCommunication(id, CommunicationStatus.CANCELED)
+        Communication cancelResult = repository.updateCommunication(id, CommunicationStatus.CANCELED)
             .orElseThrow(() -> new IllegalStateException("Ocorreu um erro ao cancelar a comunicação."));
+
+        log.info("Comunicação cancelada com sucesso.");
+
+        return cancelResult;
     }
 }
